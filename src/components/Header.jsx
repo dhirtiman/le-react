936ed58895c64d3402eae33dabefe143c4307f92
dhirtiman <dhirtimankhati@gmail.com>
2024-11-17 11:45:46 +0545

    routing useUsing useNavigate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,26 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+
+import { useNavigate } from "react-router-dom"
 
 export default function Header() {
+
+    const navigate = useNavigate();
+
+
+
     return (
         <div>
             routing and shi
-            <Link to="/">Landing</Link>
-            <Link to="/dashboard">Dashboard</Link>
+            <br></br>
+            {/* <Link to="/">Landing</Link>
+            <Link to="/dashboard">Dashboard</Link> */}
+
+            <button onClick={()=>{
+                navigate('/')    }}>Landing</button>
+
+            <button onClick={()=>{
+                navigate('/dashboard')
+            }}>Dashboard</button>
         </div>
     )
 }
