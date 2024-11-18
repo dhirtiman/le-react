02419f2242970ba534c5c6ba3a00eb7dcb8457d4
dhirtiman <dhirtimankhati@gmail.com>
2024-11-18 19:16:38 +0545

    context-api-basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
+import { useState } from 'react'
 import './App.css'
-
-
+import { useContext } from 'react'
+import { CountContext } from './contexts/CountContext'
 
 function App() {
+    const [count, setCount] = useState(0)
     return (
         <>
-
- 
+            <CountContext.Provider value={{ count, setCount }}>
+                <Count />
+            </CountContext.Provider>
         </>
     )
 }
-   
+
+function Count() {
+    return (
+        <>
+            <CountRenderer />
+            <Buttons />
+        </>
+    )
+}
+
+function CountRenderer() {
+    const { count } = useContext(CountContext)
+
+    return <p>{count}</p>
+}
+
+function Buttons() {
+    const { setCount } = useContext(CountContext)
+
+    return (
+        <>
+            <button
+                onClick={() => {
+                    setCount((count) => count + 1)
+                }}
+            >
+                +
+            </button>
+            <button
+                onClick={() => {
+                    setCount((count) => count - 1)
+                }}
+            >
+                -
+            </button>
+        </>
+    )
+}
 
 export default App
